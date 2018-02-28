31f2fd6df21da5d635c1f07c1e85ca0446ea93fb
Isaac Daniel Batista <disa.indus@gmail.com>
2018-02-28 18:41:10 +0000

    Unit 4 ok

---
+++ app/code/Unit4/Retailer/Model/Retailer.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Unit4\Retailer\Model;
+
+
+use Magento\Framework\Model\AbstractModel;
+
+class Retailer extends AbstractModel
+{
+    protected $region = null;
+    /**
+     * @return void
+     */
+    protected function _construct()
+    {
+        $this->_init(\Unit4\Retailer\Model\ResourceModel\Retailer::class);
+    }
+
+    public function getRegionCode()
+    {
+        if (!$this->region) {
+            $this->region = $this->_getResource()
+                ->getRegionCode($this->getRegionId());
+        }
+        return $this->region;
+    }
+
+    public function getAssociatedProducts() {
+        return $this->_getResource()
+            ->getAssociatedProducts($this->getId());
+    }
+}
